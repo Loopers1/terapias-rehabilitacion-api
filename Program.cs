a72ea30ee8006afce750acf78dcbf48f9b9d73e0
Loopers1 <fbtvasquez123@gmail.com>
2026-05-06 12:21:11 -0400

    fix cors and api url

--- Program.cs
+++ Program.cs
@@ -23,14 +23,6 @@ builder.Services.AddCors(options =>
     });
 });
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll",
-        builder => builder.AllowAnyOrigin()
-                          .AllowAnyMethod()
-                          .AllowAnyHeader());
-});
-
 var app = builder.Build();
 
 app.UseCors("PermitirTodo");
